a92713cbd4be9595842dae348c75160ed0f093ea
luca saintecroix <luca.saintecroix@gmail.com>
2022-11-22 17:06:38 +0100

    modified:   taxifare/interface/main.py

--- taxifare/interface/main.py
+++ taxifare/interface/main.py
@@ -277,7 +277,7 @@ def pred(X_pred: pd.DataFrame = None) -> np.ndarray:
 
 if __name__ == '__main__':
     preprocess()
-    preprocess(source_type='val')
+    # preprocess(source_type='val')
     train()
     pred()
     evaluate()
